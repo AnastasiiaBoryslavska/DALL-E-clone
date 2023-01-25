3d08eb5e96b40557b83c65df92e25845ac92ed20
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2023-01-25 18:51:13 -0500

    updated url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("https://dall-e-uv4z.onrender.com/api/v1/post", {
+        const response = await fetch("https://dall-e-clone-t6l5.onrender.com/api/v1/post", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
